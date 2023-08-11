7d708539473c0e8fa40068e526a58f8e6de47084
Alina Krmer <122799983+AlinaKrmer@users.noreply.github.com>
2023-08-11 09:22:47 +0200

    PseudoAbsenceData.R

--- Training_Validation/PseudoAbsenceData.R
+++ Training_Validation/PseudoAbsenceData.R
@@ -1,66 +1,62 @@
+# Clear environment and load required libraries
 rm(list=ls())
 library(dismo)
+library(rgeos)
+library(sp)
+library(dplyr)
 
-ac <- read.csv("/Volumes/Elements/Masterarbeit/Git/Data/Referencedata/Input/abundances.csv", header=T, sep=",", dec=",")
+# Read Data
+ac <- read.csv("/Volumes/Elements/Masterarbeit/Git/Data/Referencedata/Input/abundances.csv", header=TRUE, sep=",", dec=",")
 mask <- raster("/Volumes/Elements/Masterarbeit/Git/Data/Predictors/srtm10.tif")
-nrow(ac)
 
+# Set a random seed for reproducibility
 set.seed(1963)
 
-ac<-ac[complete.cases(ac), ]
-coordinates(ac) <- ~Lon+Lat
+# Remove incomplete cases from abundance data
+ac <- ac[complete.cases(ac), ]
+
+# Create a SpatialPointsDataFrame from abundance data
+coordinates(ac) <- ~Lon + Lat
 projection(ac) <- CRS('+proj=longlat +datum=WGS84')
 
-# circles with a radius of 150 km
-library(rgeos)
-x <- circles(ac, d=150000, lonlat=TRUE) 
-#plot(x)
+# Create circles with a radius of 150 km
+x <- circles(ac, d = 150000, lonlat = TRUE)
 pol <- polygons(x)
-#plot(pol)
 
-# circles with a radius of 50 m
-x2 <- circles(ac, d=50, lonlat=TRUE) 
-#plot(x2)
+# Create circles with a radius of 50 m
+x2 <- circles(ac, d = 50, lonlat = TRUE)
 pol2 <- polygons(x2)
-#plot(pol2)
 
 
-
-library(sp)
-
-# Generiere Punkte innerhalb von mask
-#sample_points <- randomPoints(mask, nrow(ac))
-sample_points  <- spsample(pol, nrow(ac), type='random')
-
-# Wandele die Matrix in ein SpatialPoints-Objekt um
+# Generate sample points within the mask using the generated polygons
+sample_points <- spsample(pol, nrow(ac), type='random')
 sample_points_sp <- SpatialPoints(sample_points, proj4string = CRS(proj4string(mask)))
 
-# Überprüfe, ob die Punkte innerhalb der Polygone liegen
+
+# Check if the points are inside the polygons
 inside_pol <- over(sample_points_sp, pol2)
 
-# Filtere die Punkte, die außerhalb der Polygone liegen
+# Filter out points outside the polygons
 filtered_points <- sample_points[is.na(inside_pol), ]
 
-# Plot der gefilterten Punkte
-plot(filtered_points)
-
+# Convert filtered points to a data frame
 pa <- data.frame(filtered_points)
-#write.csv(pa,"/Volumes/Elements/Masterarbeit/Git/Data/Referencedata/Input/pseudoabsence50km20230107.csv")
-pa$pa<-"Absence"
-nrow(pa)
-nrow(ac)
 
-ac <- read.csv("/Volumes/Elements/Masterarbeit/Git/Data/Referencedata/Input/abundances.csv", header=T, sep=",", dec=",")
-names(ac)
-names(pa)
+# Add a column indicating "Absence"
+pa$pa <- "Absence"
+
+# Read abundance data again
+ac <- read.csv("/Volumes/Elements/Masterarbeit/Git/Data/Referencedata/Input/abundances.csv", header=TRUE, sep=",", dec=",")
+
+# Select specific columns from abundance data
 df <- data.frame(ac[,c(3,4,5)])
 
-names(pa)<-c("Lon","Lat","abundance")
+# Rename columns of the pseudo-absence data frame and combine abundance data and pseudo absence data
+names(pa) <- c("Lon", "Lat", "abundance")
+traindat <- rbind(df, pa)
 
-traindat = rbind(df,pa)
 write.csv(traindat,"/Volumes/Elements/Masterarbeit/Git/Data/Referencedata/Input/traindat.csv")
 
-traindat %>% group_by(abundance) %>% summarise(count=n())
 
 
 
